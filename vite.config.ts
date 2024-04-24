913b03d43f39dcb3961d655d6b15fa71c30ab148
Yassine <yassine.karim.nova@gmail.com>
2024-04-24 10:59:51 +0100

    added vite checker (display errors on terminal)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"', // for example, lint .ts & .tsx
+      },
+      // to add typescript checks also
+      //typescript: true,
+    }),
+  ],
+});
