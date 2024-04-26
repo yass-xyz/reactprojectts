654b4e86df57fe2e60e7841cc1e9d3b629364444
Yassine <yassine.karim.nova@gmail.com>
2024-04-26 15:07:07 +0100

    axios config

---
+++ src/api/axios.ts
@@ -0,0 +1,36 @@
+import Axios, { InternalAxiosRequestConfig } from "axios";
+
+import { useNotification } from "@/components/hooks";
+import storage from "@/utils/storage";
+const API_URL = process.env.REACT_APP_API_URL as string;
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  const token = storage.getToken();
+  if (token) {
+    config.headers.authorization = `${token}`;
+  }
+  config.headers.Accept = "application/json";
+  return config;
+}
+
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.request.use(authRequestInterceptor);
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const { notify } = useNotification();
+    const msg = error.response?.data?.message || error.message;
+    notify({
+      msg,
+      type: "error",
+      //   title: "Error",
+    });
+
+    return Promise.reject(error);
+  }
+);
